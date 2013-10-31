18a4489f398241a2c547f451091f4f586c72ec28
asides <yaroslav.str@gmail.com>
2013-10-31 13:11:59 +0200

    fix current_default

--- app/models/d_validator.rb
+++ app/models/d_validator.rb
@@ -1,8 +1,10 @@
 class DValidator < ActiveModel::Validator
   def validate(record)
     current_default = record.class.where(is_default: true).first
-    if record.is_default and current_default.id != record.id
-      record.errors[:base] << "Only one record may be default! Current default - id:#{current_default.id} name:#{current_default.name}"
+    if current_default
+      if record.is_default and current_default.id != record.id
+        record.errors[:base] << "Only one record may be default! Current default - id:#{current_default.id} name:#{current_default.name}"
+      end
     end
   end
 end
